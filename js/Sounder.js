38106c23e49ea994fbfb2f6a409913a42f1b04c1
Joe Strong <joestrong@gmail.com>
2016-02-06 19:33:59 +0000

    Allowed for sounds to play quickly next to each other

--- js/Sounder.js
+++ js/Sounder.js
@@ -4,24 +4,16 @@ export default class Sounder
     constructor()
     {
         this.soundPath = 'sounds';
-        this.soundFiles = {
+        this.sounds = {
             'hit': 'water-drum-1.ogg'
         };
-        this.loadSounds();
-    }
-
-    loadSounds()
-    {
-        this.sounds = [];
-        for (let name in this.soundFiles) {
-            this.sounds[name] = new Audio('/' + this.soundPath + '/' + this.soundFiles[name]);
-        }
     }
 
     play(sound)
     {
         if (this.sounds[sound]) {
-            this.sounds[sound].play();
+            let audio = new Audio('/' + this.soundPath + '/' + this.sounds[sound]);
+            audio.play();
         }
     }
 }
